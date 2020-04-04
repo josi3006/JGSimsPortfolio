5be884c429695bd4a5200b9333fc357b38315181
JGSims <josi3006@hotmail.com>
2020-04-04 17:36:06 -0400

    Splash text rendering properly

--- jgs-portfolio/src/components/pages/Home.js
+++ jgs-portfolio/src/components/pages/Home.js
@@ -7,46 +7,70 @@ class Home extends React.Component {
     };
 
 
+    loopText1 = () => {
 
+        const text1 = "Full-Stack Web Developer with business leadership and coding skills."
+        const textArr1 = text1.split("");
+        let renderArr1 = [];
 
+        for (let i = 0; i < textArr1.length; i++) {
+            setTimeout(function () {
+                renderArr1.push(textArr1[i]);
+                document.getElementById("homeBody1").innerHTML = (renderArr1.join(""));
+            }, 50 * i);
+        }      
+    }
 
-    loopText = () => {
+    loopText2 = () => {
 
-        const textBody = "Here I am.";
-        const textArr = textBody.split("");
-        let renderArr = [];
+        const text2 = "Seeking opportunity to add value to your organization."
+        const textArr2 = text2.split("");
+        let renderArr2 = [];
 
-
-        for (let i = 0; i < textArr.length; i++) {
-
-            // setTimeout(function () {
-
-            setTimeout = () => {
-
-
-                console.log(textArr[i]);
-
-                renderArr.push(textArr[i]);
-
-                console.log(renderArr);
-
-                this.setState({ text: renderArr });
-            }, 100 * i);
+        for (let i = 0; i < textArr2.length; i++) {
+            setTimeout(function () {
+                renderArr2.push(textArr2[i]);
+                document.getElementById("homeBody2").innerHTML = (renderArr2.join(""));
+            }, 50 * i);
         }
-
     }
 
 
+    loopText3 = () => {
 
+        const text3 = "Coding savvy. Corporate maturity. Granville Sims."
+        const textArr3 = text3.split("");
+        let renderArr3 = [];
 
+        for (let i = 0; i < textArr3.length; i++) {
+            setTimeout(function () {
+                renderArr3.push(textArr3[i]);
+                document.getElementById("homeBody3").innerHTML = (renderArr3.join(""));
+            }, 50 * i);
+        }
+    }
+
+allText = () => {
+
+    setTimeout(this.loopText1, 1000);
+    setTimeout(this.loopText2, 4400);
+    setTimeout(this.loopText3, 9100);
+}
+
+componentDidMount(){
+    this.allText();
+  }
 
 
     render() {
         return (
             <div>
                 <h1> Home Page </h1>
-                <p><button onClick={this.loopText}>Click</button></p>
-                <p>{this.state.text}</p>
+                <div id="homeBody1" className="text-center"></div>
+                <div id="homeBody2" className="text-center"></div>
+                <div id="homeBody3" className="text-center"></div>
+
+
             </div >
         );
 
